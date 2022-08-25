ea028afaef935a51177b8029b0d8038ef5570787
niemand20 <zahid17290@gmail.com>
2022-08-25 13:53:09 +0100

    template string

---
+++ templatre.js
@@ -0,0 +1,17 @@
+const person = "adam sandler";
+const person2 = "walter white";
+const person3 = `jesse pinkman`;
+// console.log(person,person2, person3);
+
+const multiLine = `first line of text
+second line of text
+third line of text
+`;
+// console.log(multiLine)
+
+const a = 20;
+const b = 10;
+const sum = "sum of a "+ a +" and b "+ b + " is "+(a+b);
+// console.log(sum); 
+const newSum = `sum of ${a} and ${b} is ${a+b}`;
+console.log(newSum);
