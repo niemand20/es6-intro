1c91d6ae9ed13b032d386f1151bec6881de72cd3
niemand20 <zahid17290@gmail.com>
2022-09-01 13:01:40 +0100

    33-3 International Travel, Display countries name, capital,population, flags etc..

---
+++ api-examples-and-edge-cases/js/countries.js
@@ -0,0 +1,36 @@
+const loadCountries = ()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then (data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    const countryContainer =  document.getElementById("countries-container");
+    // for (const country of countries){
+        countries.forEach(country => {
+        // console.log(country);
+        const countriesDiv = document.createElement('div');
+        countriesDiv.classList.add("countries-class");
+        countriesDiv.innerHTML=`
+        <h3>Name: ${country.name.common}</h3>
+        <h3>Capital: ${country.capital?country.capital[0]:"No Capital"}</h3>
+        <h3>Currency: ${country.currencies.name}</h3>
+        <h3>Time Zone: ${country.timezones}</h3>
+        <h3>Population: ${country.population}</h3>
+        <h3>Continent: ${country.continents}</h3>
+        <h3>Flag: ${country.flag}</h3>
+       
+        
+        `
+         
+        countryContainer.appendChild(countriesDiv);
+
+        })
+        
+    
+}
+
+
+
+
+loadCountries();
