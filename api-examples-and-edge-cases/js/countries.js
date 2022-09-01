1a4c00d0b31cf7dae14bbc74c30ed77d897b6a89
niemand20 <zahid17290@gmail.com>
2022-09-01 13:54:40 +0100

    33-4 (advanced) Call dynamic API, load dynamic data to display, displaying country details

--- api-examples-and-edge-cases/js/countries.js
+++ api-examples-and-edge-cases/js/countries.js
@@ -14,14 +14,14 @@ const displayCountries = countries => {
         countriesDiv.innerHTML=`
         <h3>Name: ${country.name.common}</h3>
         <h3>Capital: ${country.capital?country.capital[0]:"No Capital"}</h3>
-        <h3>Currency: ${country.currencies[Object.keys(country.currencies)].name}</h3>
+        
         <h3>Time Zone: ${country.timezones}</h3>
         <h3>Population: ${country.population}</h3>
         <h3>Continent: ${country.continents}</h3>
         <h3>Flag: ${country.flag}</h3>
-       <button class="button" onclick=" loadCountryDetails()">Details</button>
+       <button class="button" onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `
-         
+        // <h3>Currency: ${country.currencies[Object.keys(country.currencies)].name}</h3>
         countryContainer.appendChild(countriesDiv);
 
         })
@@ -29,9 +29,25 @@ const displayCountries = countries => {
     
 }
 
-const loadCountryDetails = ()=>{
-    console.log("get country details")
+const loadCountryDetails = (code)=>{
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log("get country details", code)
+    // console.log(url);
+    fetch(url)
+    .then(res=> res.json())
+    .then(data => displayCountriesDetails(data[0]))
 }
 
+
+const displayCountriesDetails = country =>{
+    console.log(country);
+    const countryDetail = document.getElementById("country-detail");
+    countryDetail.innerHTML = `
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}" alt="" />
+    `
+
+}
 
 loadCountries();
