2576a2f3f2a5e0338a173dc7f7e7789d8afc965f
niemand20 <zahid17290@gmail.com>
2022-09-02 01:35:20 +0100

    searching by first letter or word

--- api-examples-and-edge-cases/js/meals.js
+++ api-examples-and-edge-cases/js/meals.js
@@ -1,5 +1,6 @@
-const loadMeals=() =>{
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+const loadMeals= search =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
 }
@@ -7,20 +8,22 @@ const loadMeals=() =>{
 const displayMeals = meals =>{
     const mealsContainer = document.getElementById("meal-container");
     meals.forEach(meal=>{
+        console.log(meal);
         const mealDiv = document.createElement("div");
         mealDiv.classList.add("col");
         mealDiv.innerHTML =`
         <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
-                       <h5 class="card-title">Card title</h5>
-                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                       <h5 class="card-title">Meal ${meal.strMeal}</h5>
+                       <p class="card-text">Details: ${meal.strInstructions.slice(0,200)}</p>
                     </div>
                   </div>
         `
         mealsContainer.appendChild(mealDiv);
-    })
-}
+    });
+};
 
 
-loadMeals();
+loadMeals("rice");
+// loadMeals("fish");
