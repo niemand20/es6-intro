1022f563fbd93278a6cc5c0ef62fb297704ff262
niemand20 <zahid17290@gmail.com>
2022-09-04 00:41:57 +0100

    34-3 Toggle no phone found message based on search result

--- phone-hunter/js/app.js
+++ phone-hunter/js/app.js
@@ -9,6 +9,17 @@ const loadPhones = async(searchText)=>{
 const displayPhones = phones =>{
     const phonesContainer = document.getElementById("phones-container");
     phonesContainer.textContent="";
+    //display 20 phones only
+    phones = phones.slice(0,20);
+    //display no phone Found
+    const noPhone = document.getElementById("no-found-message");
+    if(phones.length===0){
+        noPhone.classList.remove("d-none")
+    }
+    else{
+        noPhone.classList.add("d-none")
+    }
+    //display all phone found
     phones.forEach(phone  =>{
         const phoneDiv = document.createElement("div");
             phoneDiv.classList.add("col");
@@ -35,4 +46,4 @@ document.getElementById("btn-search").addEventListener("click", function (){
 })
 
 
-loadPhones();
+// loadPhones();
