8faef37fbd492fbd7b7e3be4693111fe72a4d3c6
niemand20 <zahid17290@gmail.com>
2022-09-07 12:22:21 +0100

    object keys, values and pairs

---
+++ explore-js-ojects/freeze.js
@@ -0,0 +1,14 @@
+const bottle = {
+    color:"yellow",
+    price: 50,
+    isCleaned: true,
+    capacity: 1
+}
+
+const keys = Object.keys(bottle);
+// console.log(keys);
+const values = Object.values(bottle);
+// console.log(values);
+
+const pairs = Object.entries(bottle);
+//console.log(pairs);
